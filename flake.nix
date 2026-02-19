bd2c0fb1552fd9af8dd346210be21d7f2e77cdd0
Guilhem Saurel <guilhem.saurel@laas.fr>
2026-02-19 20:06:41 +0100

    lib

--- flake.nix
+++ flake.nix
@@ -2,150 +2,93 @@
   description = "Whole Body Model Predictive Control in the AGIMUS architecture";
 
   inputs = {
-    gepetto.url = "github:gepetto/nix";
-    flake-parts.follows = "gepetto/flake-parts";
-    nixpkgs.follows = "gepetto/nixpkgs";
-    nix-ros-overlay.follows = "gepetto/nix-ros-overlay";
-    systems.follows = "gepetto/systems";
-    treefmt-nix.follows = "gepetto/treefmt-nix";
+    gazebros2nix.url = "github:gepetto/gazebros2nix";
+    flake-parts.follows = "gazebros2nix/flake-parts";
+    nixpkgs.follows = "gazebros2nix/nixpkgs";
+    nix-ros-overlay.follows = "gazebros2nix/nix-ros-overlay";
+    systems.follows = "gazebros2nix/systems";
+    treefmt-nix.follows = "gazebros2nix/treefmt-nix";
   };
 
   outputs =
     inputs:
-    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = import inputs.systems;
-      imports = [ inputs.gepetto.flakeModule ];
-      gepetto-pkgs.overlays =
-        let
-          inherit (inputs.nixpkgs) lib;
-        in
-        [
-          (_final: prev: {
-            pythonPackagesExtensions = prev.pythonPackagesExtensions ++ [
-              (_python-final: python-prev: {
-                agimus-controller = python-prev.agimus-controller.overrideAttrs {
-                  src = lib.fileset.toSource {
-                    root = ./.;
-                    fileset = lib.fileset.unions [
-                      ./agimus_controller
-                    ];
-                  };
-                  # Remove when the CI passes. And update https://github.com/Gepetto/nix
-                  nativeCheckInputs = [
-                    prev.python3Packages.pytest
-                    prev.python3Packages.pytestCheckHook
-                  ];
-                  doCheck = true;
-                };
-                agimus-controller-examples = python-prev.agimus-controller-examples.overrideAttrs {
-                  src = lib.fileset.toSource {
-                    root = ./.;
-                    fileset = lib.fileset.unions [
-                      ./agimus_controller_examples
-                    ];
-                  };
-                };
-              })
-            ];
-            rosPackages =
-              let
-                src = lib.fileset.toSource {
-                  root = ./.;
-                  fileset = lib.fileset.unions [
-                    ./agimus_controller_ros
-                  ];
-                };
-                nativeCheckInputs = [
-                  prev.python3Packages.pytest
-                  prev.python3Packages.pytestCheckHook
-                ];
-                doCheck = true;
-              in
-              prev.rosPackages
-              // {
-                humble = prev.rosPackages.humble.overrideScope (
-                  _humble-final: humble-prev: {
-                    agimus-controller-ros = humble-prev.agimus-controller-ros.overrideAttrs {
-                      inherit
-                        src
-                        doCheck
-                        nativeCheckInputs
-                        ;
-                    };
-                  }
-                );
-                jazzy = prev.rosPackages.jazzy.overrideScope (
-                  _jazzy-final: jazzy-prev: {
-                    agimus-controller-ros = jazzy-prev.agimus-controller-ros.overrideAttrs {
-                      inherit
-                        src
-                        doCheck
-                        nativeCheckInputs
-                        ;
-                    };
-                  }
-                );
-              };
-          })
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } (
+      { lib, self, ... }:
+      let
+        distros = [
+          "humble"
+          "jazzy"
+          "kilted"
+          "rolling"
         ];
-      perSystem =
-        {
-          lib,
-          pkgs,
-          self',
-          ...
-        }:
-        {
-          devShells = {
-            default = self'.devShells.env;
-            env = pkgs.mkShell {
-              name = "env";
-              packages = [
-                (pkgs.python3.withPackages (p: [
-                  p.agimus-controller
-                  p.agimus-controller-examples
-                  p.pytest
-                ]))
-              ];
-            };
-          }
-          # 2025/11/07 Remove this env from MacOs as franka_description is
-          # written as broken
-          // lib.optionalAttrs pkgs.stdenv.hostPlatform.isLinux {
-            ros-env = pkgs.mkShell {
-              name = "ros-env";
-              packages = [
-                self'.packages.ros-env
+        packages = {
+          agimus-controller = _final: _ros-final: {
+            src = lib.fileset.toSource {
+              root = ./.;
+              fileset = lib.fileset.unions [
+                ./agimus_controller
               ];
             };
           };
-          packages = {
-            default = self'.packages.agimus-controller;
-            agimus-controller = pkgs.python3Packages.agimus-controller;
-            agimus-controller-examples = pkgs.python3Packages.agimus-controller-examples;
-          }
-          // lib.optionalAttrs pkgs.stdenv.hostPlatform.isLinux {
-            ros-humble-agimus-controller-ros = pkgs.rosPackages.humble.agimus-controller-ros;
-            ros-jazzy-agimus-controller-ros = pkgs.rosPackages.jazzy.agimus-controller-ros;
-          }
-          // lib.optionalAttrs pkgs.stdenv.hostPlatform.isLinux {
-            ros-env =
-              with pkgs.rosPackages.jazzy;
-              buildEnv {
-                name = "ros-env";
-                paths = [
-                  pkgs.python3Packages.meshcat
-                  pkgs.python3Packages.coal
-                  pkgs.python3Packages.pinocchio
-                  pkgs.python3Packages.gepetto-gui
-                  pkgs.python3Packages.agimus-controller
-                  pkgs.rosPackages.humble.franka-description
-                  pkgs.rosPackages.humble.xacro
-                  pkgs.rosPackages.humble.ament-index-python
-                  pkgs.rosPackages.humble.realsense2-description
-                ];
-              };
+
+          # agimus-controller-examples = _final: _ros-final: {
+          #   src = lib.fileset.toSource {
+          #     root = ./.;
+          #     fileset = lib.fileset.unions [
+          #       ./agimus_controller_examples
+          #     ];
+          #   };
+          # };
+
+          agimus-controller-ros = _final: _ros-final: {
+            src = lib.fileset.toSource {
+              root = ./.;
+              fileset = lib.fileset.unions [
+                ./agimus_controller_ros
+              ];
+            };
           };
+
         };
-    };
+      in
+      {
+        systems = [ "x86_64-linux" ];
+        imports = [
+          inputs.gazebros2nix.flakeModule
+          { gazebros2nix-pkgs.overlays = [ self.overlays.default ]; }
+        ];
+        flake.overlays.default = final: prev: {
+          rosPackages =
+            prev.rosPackages
+            // lib.genAttrs distros (
+              d:
+              prev.rosPackages."${d}".overrideScope (
+                ros-final: ros-prev:
+                lib.mapAttrs (
+                  package: override: ros-prev.${package}.overrideAttrs (override final ros-final)
+                ) packages
+
+              )
+            );
+        };
+        perSystem =
+          { pkgs, self', ... }:
+          {
+            packages = {
+              default = self'.packages.ros-rolling-agimus-controller-ros;
+            }
+            // lib.listToAttrs (
+              lib.mapCartesianProduct
+                (
+                  { distro, package }:
+                  lib.nameValuePair "ros-${distro}-${package}" pkgs.rosPackages."${distro}"."${package}"
+                )
+                {
+                  distro = distros;
+                  package = lib.attrNames packages;
+                }
+            );
+          };
+      }
+    );
 }
